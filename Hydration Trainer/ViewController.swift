8bb92a70b4c065dc8b96596038e3bb304303993b
Ian Kohlert <iankohlert@gmail.com>
2016-10-25 13:05:40 -0600

    Initial Commit

---
+++ Hydration Trainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hydration Trainer
+//
+//  Created by Ian Kohlert on 2016-10-25.
+//  Copyright © 2016 Ian Kohlert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
